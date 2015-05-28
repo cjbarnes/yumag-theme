0c17dd923e37037dc1e15be46e065533f4c97df2
chris barnes <mail@cjbarnes.co.uk>
2015-05-28 17:41:57 +0100

    Better 'no results' layout for On the Grapevine

---
+++ content-none-yumag_notice.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template part for displaying a message that Notices cannot be found.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package YuMag
+ */
+?>
+
+<section class="entry single-entry category-all-about-yu no-results">
+	<div class="entry-above-footer single-entry-above-footer">
+
+		<div class="entry-content">
+			<p class="lead"><?php _e( 'Sorry, but nothing matched your search options. Please try again.', 'yumag' ); ?></p>
+		</div><!-- .entry-content -->
+
+	</div><!-- .entry-above-footer -->
+</section><!-- .no-results -->
